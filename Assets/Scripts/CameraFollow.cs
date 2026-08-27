50874c3d956f4e73ffc871b7dbeb7563abc1bcbc
Isaac J <88879000+fnfplayer64@users.noreply.github.com>
2026-08-27 11:21:07 +1000

    wip

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -3,7 +3,9 @@ using UnityEngine;
 public class CameraFollow : MonoBehaviour
 {
     public Transform target;
-    public Vector3 offset;
+
+    // Use the Z you want here
+    public Vector3 offset = new Vector3(0f, 0f, -27.6f);
 
     void LateUpdate()
     {
